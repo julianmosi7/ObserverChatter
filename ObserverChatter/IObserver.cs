4992514f0cb8ee40b82d4566fcbcb49a27fcae01
Julian <moshammer.ju@gmail.com>
2020-12-10 15:50:34 +0100

    init

---
+++ ObserverChatter/IObserver.cs
@@ -0,0 +1,11 @@
+﻿namespace ObserverChatter
+{
+    public interface IObserver
+    {
+        string ClientName { get; }
+
+        void ClientAttached(string name);
+        void ClientDetached(string name);
+        void Update(string name, string msg);
+    }
+}
---
+++ ObserverChatter/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverChatter
+{
+    abstract class Subject
+    {
+        List<IObserver> observers = new List<IObserver>();
+
+        public int NrObservers { get; }
+        public string Topic { get; set; }
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Notify(string name, string msg)
+        {
+            observers.ForEach(x => x.Update(name, msg));
+        }
+    }
+}
